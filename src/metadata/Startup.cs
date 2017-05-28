af415f0db9cd7a104c262e4710a83f37d1c96a94
Demis Bellot <demis.bellot@gmail.com>
2017-05-28 15:30:37 -0400

    Add scratch project to test latest .NET Core snippets

---
+++ src/metadata/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Funq;
+using ServiceStack;
+
+namespace scratch
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseServiceStack(new AppHost());
+
+            app.Run(context =>
+            {
+                context.Response.Redirect("/metadata");
+                return Task.FromResult(0);
+            });
+        }
+    }
+
+
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    [Api("Test request")]
+    [Route("/test/{Input}", "GET")]
+    [Route("/test")]
+    public class TestRequest : IReturn<TestResponse>
+    {
+        [ApiMember(Name = "Parameter name", Description = "Parameter Description",
+            ParameterType = "body", DataType = "string", IsRequired = true)]
+        public string Input { get; set; }
+    }
+    public class TestResponse
+    {
+        public string Output { get; set; }
+    }
+
+    public class MyServices : Service
+    {
+        public object Any(Hello request) =>
+            new HelloResponse { Result = $"Hello, {request.Name}!" };
+
+        public object Any(TestRequest request) => new TestResponse();
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("ServiceStack + .NET Core Web App", typeof(MyServices).GetAssembly()) { }
+
+        public override void Configure(Container container)
+        {
+        }
+    }
+}
