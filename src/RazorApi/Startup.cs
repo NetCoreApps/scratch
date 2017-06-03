76ed5baf23da23824918883cd6330bfefa323874
Demis Bellot <demis.bellot@gmail.com>
2017-06-03 19:04:11 -0400

    Add Test RazorApi MVC project

---
+++ src/RazorApi/Startup.cs
@@ -0,0 +1,111 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ServiceStack;
+using ServiceStack.Mvc;
+
+namespace RazorApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseServiceStack(new AppHost());
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class MyServices : Service
+    {
+        public object Any(Hello request) => 
+            new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+
+    public class Test
+    {
+        public string ExternalId { get; set; }
+    }
+
+    [Route("/test")]
+    public class TestGet : IGet, IReturn<Test>
+    {
+    }
+
+    public class TestService : Service
+    {
+        public Test Get(TestGet request)
+        {
+            var test = new Test { ExternalId = "abc" };
+            return test;
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost() 
+            : base("ServiceStack + .NET Core", typeof(MyServices).GetTypeInfo().Assembly) {}
+
+        public override void Configure(Funq.Container container)
+        {
+            SetConfig(new HostConfig
+            {
+                HandlerFactoryPath = "api",
+                DebugMode = true,
+            });
+
+            Plugins.Add(new RazorFormat());
+        }
+    }
+}
